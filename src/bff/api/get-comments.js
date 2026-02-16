deadff6d682a5dc908a42e667c111b1dd37aaa07
STAP16 <stepancat61@gmail.com>
2026-02-16 15:45:54 +0300

    Фикса Json-server - Проблемма в версии

--- src/bff/api/get-comments.js
+++ src/bff/api/get-comments.js
@@ -5,8 +5,7 @@ const POST_COMMENTS_URL = 'http://localhost:3000/comments?post_id='
 
 export const getComments = async postId => {
 	const url = postId === undefined ? ALL_COMMENTS_URL : POST_COMMENTS_URL + String(postId)
-	console.log(postId)
-	console.log(url)
+
 	return fetch(url).then(loadedComments =>
 		loadedComments
 			.json()
